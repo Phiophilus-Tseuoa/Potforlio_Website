1d954b0455a83e387ec809d871bbfc7272fcf965
Phiophilus Tseuoa <siyabongamzizi@icloud.com>
2026-06-29 22:09:59 +0200

    Refactor skills section to use data file

---
+++ src/Data/skills.js
@@ -0,0 +1,45 @@
+const skills = [
+  {
+    title: "Programming Languages",
+    skills: [
+      "Python",
+      "Java",
+      "JavaScript",
+      "C",
+      "C++",
+      "SQL",
+    ],
+  },
+  {
+    title: "Web Development",
+    skills: [
+      "HTML5",
+      "CSS3",
+      "React",
+      "Tailwind CSS",
+      "Streamlit",
+    ],
+  },
+  {
+    title: "Data Science",
+    skills: [
+      "Pandas",
+      "NumPy",
+      "Plotly",
+      "Data Visualization",
+    ],
+  },
+  {
+    title: "Tools & Learning",
+    skills: [
+      "Git",
+      "GitHub",
+      "VS Code",
+      "Linux",
+      "TryHackMe",
+      "Cybersecurity",
+    ],
+  },
+];
+
+export default skills;
